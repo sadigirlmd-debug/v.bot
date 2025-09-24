7a73b9d89e99792e48f47a2581c12df2dababdc6
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-25 04:15:19 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -999,22 +999,29 @@ else if(os.hostname().length == 8) hostname = 'koyeb'
 else hostname = os.hostname()
 let monspace ='```'
 let monspacenew ='`'
-const cap = `${monspace}👋 කොහිමද ${pushname} I'm alive now${monspace}
-    
+const cap = `${monspace}😚 කොහොමද ${pushname} I'm alive now${monspace}
+*🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️*
+
 *🚀Version:* ${require("../package.json").version}
+
 *⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+
 *🕒Runtime:* ${runtime(process.uptime())}
+
 *📍Platform:* ${hostname}
 
-🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
-                    
-*🌻Have A Nice Day..*🌻`
+*🤖sᴛᴀᴛᴜs*: ᴢᴀɴᴛᴀ-xᴍᴅ ᴀʟɪᴠᴇ ᴀɴᴅ ʀᴇᴀᴅʏ
+
+
+🖇️ *CHANEL :- https://whatsapp.com/channel/0029Vb4F314CMY0OBErLlV2M*
+
+👤 *OWNER :- MR SURANGA MOD-Z*`
 
 var vajiralod = [
 "LOADING ●●○○○○",
 "LOADING ●●●●○○",
 "LOADING ●●●●●●",
-"`COMPLETED ✅`"	
+"`🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️`"	
 ]
 let { key } = await conn.sendMessage(from, {text: ''})
 
