6ca243a8a2380d22b97d3ffa063d09d60a0736be
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-25 00:22:17 +0530

    Update tool-imgurl.js

--- plugins/tool-imgurl.js
+++ plugins/tool-imgurl.js
@@ -1,3 +1,51 @@
+const config = require('../settings')
+const os = require('os')
+const fs = require('fs')
+const si = require('systeminformation')
+const prefix = config.PREFIX
+const simpleGit = require('simple-git')
+const Levels = require("discord-xp")
+const git = simpleGit()
+const Heroku = require('heroku-client')
+const appname = process.env.APP_NAME || ''
+const herokuapi = process.env.HEROKU_API
+const pingSt = new Date();
+const { cmd, commands } = require('../lib/command')
+const DB = require('../lib/scraper')
+const owner = JSON.parse(fs.readFileSync('./lib/owner.json'))
+const devlopernumber = "94711453361"
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson,clockString, jsonformat} = require('../lib/functions')
+var { updateCMDStore,isbtnID,getCMDStore,getCmdForCmdId,connectdb,input,get, updb,updfb } = require("../lib/database")
+const {
+    default: makeWASocket,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
+
+
+ function genMsgId() {
+  const prefix = "3EB";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomText = prefix;
+
+  for (let i = prefix.length; i < 22; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomText += characters.charAt(randomIndex);
+  }
+
+  return randomText;
+} 
+
+const reportedMessages = {}
+//const isBan = banUser.includes(mek.sender)
+	    
+	
+var BOTOW = ''
+if(config.LANG === 'SI') BOTOW = "*ඔබ Bot\'s හිමිකරු හෝ  උපපරිපාලක නොවේ !*"
+else BOTOW = "*You are not bot\'s owner or moderator !*"
+//============================================================================
+
 cmd({
   'pattern': "tourl",
   'alias': ["imgtourl", "imgurl", "url", "geturl", "upload"],
