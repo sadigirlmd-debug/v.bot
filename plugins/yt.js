b5b8061d08dea1f9e716f59e6c0c637c9fb1afb4
webscrape2003 <webscrape2002@gmail.com>
2025-09-25 06:02:32 +0530

    Update yt.js

--- plugins/yt.js
+++ plugins/yt.js
@@ -23,7 +23,7 @@ cmd({
   dontAddCommandList: true,
   filename: __filename
 },
-async (conn, mek, m, { from, q, reply }) => {
+async (conn, mek, m, { from, q, reply, l}) => {
         try {
 await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
 if(!q) return await conn.sendMessage(from , { text: '*Need link...*' }, { quoted: mek } ) 
