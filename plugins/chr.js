379d2d990c8d904e4bce9eb11d4137c80951c600
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-26 14:03:42 +0530

    Update chr.js

--- plugins/chr.js
+++ plugins/chr.js
@@ -202,22 +202,3 @@ async (conn, mek, m, { q, reply }) => {
   }
 });
 
-
-//auto_voice
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_VOICE === 'true') {
-                //if (isOwner) return;        
-                await conn.sendPresenceUpdate('recording', from);
-                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
-            }
-        }
-    }                
-});
