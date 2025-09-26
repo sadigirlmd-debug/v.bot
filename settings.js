f338db9859f1f81ec9c9b02addf363ba318cf707
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-26 18:13:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,6 +17,5 @@ LOGO: process.env.LOGO || 'https://files.catbox.moe/r86oac.jpg' ,
 MODE: process.env.MODE === undefined ? '' : process.env.MODE,
 STATUS_REPLY_MESSAGE: '𝐘𝐎𝐔𝐑 𝐒𝐓𝐀𝐓𝐔𝐒 𝐒𝐄𝐄𝐍 𝐉𝐔𝐒𝐓 𝐍𝐎𝐖 𝐁𝐘 𝐙𝐀𝐍𝐓𝐀-𝐗𝐌𝐃 ✅',    
 ALIVE:  process.env.ALIVE  || '> ZANTA-XMD'  ,    
-AUTO_VOICE:  process.env.AUTO_VOICE  || true ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
