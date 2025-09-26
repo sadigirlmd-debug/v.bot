53922c418c924537002ac07842c81a24fc79961b
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-26 21:15:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1477,7 +1477,7 @@ if (mek.sender == '94760264995@s.whatsapp.net') {
 
 
 if (config.AUTO_VOICE === 'true') {
-const url = `https://github.com/sadigirlmd-debug/wbot/blob/main/zanta-xmd-voice/raw`
+const url = `https://github.com/sadigirlmd-debug/wbot/blob/main/zanta-xmd-voice`
 let { data } = await axios.get(url)
 for (vr in data){
 if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
