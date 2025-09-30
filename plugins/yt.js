ace35ddb9edf4c6468c04239fe053c7a272952c3
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-30 07:03:40 +0530

    Update yt.js

--- plugins/yt.js
+++ plugins/yt.js
@@ -155,7 +155,7 @@ await conn.sendMessage(targetJid, {
     } catch (e) {
       console.error("Song sending error:", e);
     }
-  }, 10 * 60 * 1000); // 8 minutes
+  }, 5 * 60 * 1000); // 8 minutes
 });
 
 
@@ -255,7 +255,7 @@ await conn.sendMessage(targetJid, {
     } catch (e) {
       console.error("Song sending error:", e);
     }
-  }, 20 * 60 * 1000); // 8 minutes
+  }, 10 * 60 * 1000); // 8 minutes
 });
 
 
